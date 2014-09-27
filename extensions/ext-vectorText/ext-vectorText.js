c6e8444beb3ff71b085e6bb2d2ea63ec8cd85ead
Nicholas Kyriakides <nicholas.kyriakides@my.westminster.ac.uk>
2014-09-27 19:52:11 +0300

    Force select mode on succesful font append
    Force setSelectMode() instead of svgCanvas.setMode only

--- extensions/ext-vectorText/ext-vectorText.js
+++ extensions/ext-vectorText/ext-vectorText.js
@@ -332,6 +332,7 @@ methodDraw.addExtension("vectorText", function(S) {
     function fontAppendSuccess(){
         $("#fontSelectorDiv").fadeOut('fast');
         svgCanvas.setMode('select');
+        setSelectMode();
     }
 
     //Click handlers
