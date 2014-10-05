46da59dbec0e4ff743133d59f72da2135fa86532
Nicholas Kyriakides <nicholas.kyriakides@my.westminster.ac.uk>
2014-10-06 00:54:57 +0300

    minor commit - prepare for big commit

--- extensions/ext-translations/ext-translations.js
+++ extensions/ext-translations/ext-translations.js
@@ -2,11 +2,60 @@
 "use strict";
 
 
-var translations = {};
+
+/*
+ * ext-translations.js
+ *
+ * Proprietary software
+ *
+ * Copyright(c) 2014 Nicholas Kyriakides
+ *
+ *
+ * Dependencies:
+ * 1) jQuery
 
 
-var translations.de;
-translations.de.file = "Datei";
+
+IMPORTANT NOTE: This extension does not conform at all to the guide for creating extensions for SVG-edit
+
+-This extension is split in 5 sections
+
+1) Defining extesion and jQuery helper function
+2) Define global variables
+3) Define static HTML elements needed for extension
+4) Define functions needed for extension
+5) Return object with ability to use svg-edit events
+
+ */
+"use strict";
+
+
+// Section 1) Define extension --------------------------------------------------------------------------------------------------------------------------------------
+
+methodDraw.addExtension("translations", function(S) {
+    var svgcontent = S.svgcontent,
+        svgns = "http://www.w3.org/2000/svg",
+        svgdoc = S.svgroot.parentNode.ownerDocument,
+        svgCanvas = methodDraw.canvas,
+        ChangeElementCommand = svgedit.history.ChangeElementCommand,
+        addToHistory = function(cmd) {
+            svgCanvas.undoMgr.addCommandToHistory(cmd);
+        };
+        $.fn.attachToPanelPosition = function(i) {
+        if(i===0){i=1};
+        i=i-1; 
+        var elems = this.find('> *');
+        if (elems.length > i) return elems.eq(i);
+        else return this;
+        }
+
+// Section 2) Global variables --------------------------------------------------------------------------------------------------------------------------------------
+
+window.onload = function(){
+var translations = {de:{}};
+
+
+translations.de.file_menu_btn = "Datei";
 translations.de.edit_menu_btn = "Edit";
 translations.de.object_menu_btn = "Objekt";
 translations.de.view_menu_btn = "Ansicht";
@@ -46,11 +95,61 @@ translations.de.formSubmitBtn = "Anfrage abschicken";
 
 
 
+// Section 3) Append necessary HTML elements (if it's a left-toolbar button define it according to svg-edit extension docs in return object.)------------------------
 
 
 
-for (var property in translations) {
-    if (translations.hasOwnProperty(property)) {
-        console.log(property)
-    }
+
+
+
+//Section 4) Functions for extension-------------------------------------------------------------------------------------------------------------------------------
+
+
+for (var prop in translations["de"]) {
+  var propertyName = prop;
+  console.log(propertyName)
+
+if($("#"+propertyName).attr('placeholder')){
+$("#"+propertyName).attr('placeholder',translations["de"][propertyName]);
+
 }
+
+else if($("#"+propertyName).attr('title')){
+$("#"+propertyName).attr('title',translations["de"][propertyName]);
+
+}
+else{
+ $("#"+propertyName).html(translations["de"][propertyName]);
+  // do something with your new propertyName
+}
+}
+
+};
+
+
+
+
+
+
+
+
+
+
+//Section 5) Extension Return object-----------------------------------------------------------------------------------------------------------------------------------
+
+    return {
+        name: "mailDesign",
+        svgicons: "extensions/vectorText-icon.xml", //this is not needed since we don't need an icon but the extension throws error without it.
+
+        /* //We dont need any events here so this is commented out.
+        
+         selectedChanged: function(multiselected) {
+        console.log("firing up a sample event, in this case selectedChangeed")
+             return;
+         }
+
+         */
+
+    }
+
+});
